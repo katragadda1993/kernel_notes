a7ea4469731e0bb30888bd5cc10598acf609c313
mastan katragadda <katragaddamastan@gmail.com>
2021-03-27 13:52:52 +0530

    socket

---
+++ linux_user_space/sockets/unp.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include <netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+
+#define MAXLINE 80
+
+#define debug(fmt, args...) {printf("Debug:"fmt"\n",##args);}
+#define err_sys(fmt, args...) {printf("SYS:"fmt"\n",##args);exit(-1);}
+#define err_exit(fmt, args...) {printf("EXIT"fmt"\n",##args);exit(-2);}
+#define err_quit(fmt, args...) {printf("QUIT:"fmt"\n",##args);exit(-3);}
